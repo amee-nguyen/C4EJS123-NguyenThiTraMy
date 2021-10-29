fe4ca5f31bcfaaacfdcf8dbbed7d5f6df837ed57
enter <ameenguyen.07@gmail.com>
2021-10-29 21:58:34 +0700

    add progress test 2

---
+++ Progress-Test-2/index.js
@@ -0,0 +1,43 @@
+const charactersList = document.getElementById('charactersList');
+const search = document.getElementById('search');
+let hpCharacters = {};
+
+search.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+    const filteredCharacters = hpCharacters.filter((character) => {
+        return (
+            character.name.toLowerCase().includes(searchString) ||
+            character.house.toLowerCase().includes(searchString)
+        );
+    });
+    displayCharacters(filteredCharacters);
+});
+
+const loadCharacters = async() => {
+    try {
+        const res = await fetch('http://hp-api.herokuapp.com/api/characters');
+        hpCharacters = await res.json();
+        displayCharacters(hpCharacters);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+
+const displayCharacters = (characters) => {
+    const htmlString = characters
+        .map((character) => {
+            return `
+            <li class="character">
+                <h2>${character.name}</h2>
+                <p>House: ${character.house}</p>
+                <img src="${character.image}"></img>
+            </li>
+        `;
+        })
+        .join('');
+    charactersList.innerHTML = htmlString;
+};
+
+loadCharacters();
